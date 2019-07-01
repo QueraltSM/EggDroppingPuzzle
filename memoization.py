c18b54e433275b72f4d1a66c7b96a9695df7890f
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-07-01 12:16:59 +0100

    Final version

--- memoization.py
+++ memoization.py
@@ -1,15 +1,14 @@
 import functools
 import sys
 
-cache={}
-def eggDrop(eggs,floors):
+def eggDrop(eggs,floors,cache):
     if eggs == 1 or floors < 2:
         return floors
     if cache.get((eggs,floors)) is not None:
         return cache[eggs,floors]
     else:
-        cache[eggs,floors] = min(1 + max(eggDrop(eggs - 1, x - 1), eggDrop(eggs, floors - x)) for x in xrange(1, floors))
+        cache[eggs,floors] = min(1 + max(eggDrop(eggs - 1, x - 1,cache), eggDrop(eggs, floors - x,cache)) for x in xrange(1, floors))
     return cache[eggs,floors]
 
 def init(eggs,floors):
-    print "Memoization = ", eggDrop(eggs, floors)
+    print "Memoization = ", eggDrop(eggs, floors,{})
